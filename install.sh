46fc62024d39bf84e50262024ae194372a660658
Gustavo Padovan <gustavo@padovan.org>
2018-02-28 11:20:40 -0300

    move to install.sh and add cmd to install rpm packages

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Install frequently used rpm packages..." 
+sudo dnf install tmux gnome-shell-extension-pomodoro perl-File-Slurp perl-Authen-SASL \
+	perl-IO-Socket-INET6 perl-Net-DNS gvim gcc-aarch64-linux-gnu gcc-arm-linux-gnu \
+	mutt gitk gcc bison flex binutils-devel automake ncurses-devel autoconf
+
+echo "Install home config files for $(whoami)..." 
+
+rm confs/*~
+
+for conf in $(ls -A confs/); do
+	echo $conf
+	ln -snf $(pwd)/confs/$conf ~ 
+done
+
+echo "...Done"
