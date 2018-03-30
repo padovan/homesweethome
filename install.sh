d1971bb3a2e9c58b0360eeceadd5eeba174e76d0
Gustavo Padovan <gustavo@padovan.org>
2018-03-30 15:45:44 -0300

    add more package to install

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Install frequently used rpm packages..."
 sudo dnf install tmux gnome-shell-extension-pomodoro perl-File-Slurp perl-Authen-SASL \
 	perl-IO-Socket-INET6 perl-Net-DNS gvim gcc-aarch64-linux-gnu gcc-arm-linux-gnu \
 	mutt gitk gcc bison flex binutils-devel automake ncurses-devel \
-	autoconf git-all ccache
+	autoconf git-all ccache elfutils-libelf-devel
 
 echo "Install home config files for $(whoami)..." 
 
